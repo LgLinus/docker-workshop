68d4fa0ac80ff531db0a3964217347c64d03e6bc
Linus Granath <linusgh@axis.com>
2020-01-20 08:37:38 +0100

    Rename res

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
+app.get('/', (req, response) => {
+  response.send('Hello World!!');
 });
 
-/*app.get("/items", (req, res) => {
+/*app.get("/items", (req, response) => {
   var mysql = require("mysql");
 
   var connection = mysql.createConnection({
@@ -21,11 +21,11 @@ app.get("/", (req, res) => {
   connection.query("SELECT * FROM items", function(error, results, fields) {
     if (error) throw error;
 
-    res.send(results);
+    response.send(results);
   });
   connection.end();
 });
 */
 app.listen(8000, () => {
-  console.log("Example app listening on port 8000!");
+  console.log('Example app listening on port 8000!');
 });
